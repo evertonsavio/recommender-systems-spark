6a3df20b450d2dfe9363bc41fc902efce25b643b
Everton Sávio S. Lucas <everluca@outlook.com>
2020-08-15 13:10:58 -0300

    Sistema de recomendacao de videos

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Sistema de recomendação usando Apache Spark com MLlib. Linguagem de programação: Java.  
   
 * Estrategia: Collaborative Filtering -> Matrix Factorization -> Alternating Least Squares (ALS)  
+  
+### Funcionamento do algoritmo para Big Data  
+  
+* Os dados usados como input contem 3 atributos {id do usuario, id do video assistido, e a proporcao do video assistido },
+a proporcao do video assistido é o quanto o usuario assistiu daquele video e é usado com um rating.  
+  
+* Através da tecnica de ALS o modelo é criado a partir do histórico dos usuários e é feito um agrupamento não supervisionado.   
+  
+* O metodo do modelo entao é aplicado aos usuarios, o numero 5 e escolhido como numero de outros videos recomendados:  
+```
+Dataset<Row> recomendacoesParaUsuario = model.recommendForAllUsers(5);
+```
+    
+* O metodo retorna um dataset onde possui o array de recomendacaoes com suas respectivas probabilidades para os proximos 5 videos para cada usuario.  
+  
+
+
--- Recomendacao/src/main/java/com/evertonsavio.java
+++ Recomendacao/src/main/java/com/evertonsavio.java
@@ -2,9 +2,15 @@ package com;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.ml.recommendation.ALS;
+import org.apache.spark.ml.recommendation.ALSModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import javax.xml.crypto.Data;
+
+import java.util.List;
 
 import static org.apache.spark.sql.functions.col;
 
@@ -28,5 +34,38 @@ public class evertonsavio {
         csvData = csvData.withColumn("proportionWatched", col("proportionWatched").multiply(100));
         csvData.show();
 
+        //csvData.groupBy("userId").pivot("courseId").sum("proportionWatched").show();
+
+        ALS als = new ALS()
+                .setMaxIter(10)
+                .setRegParam(0.1)
+                .setUserCol("userId")
+                .setItemCol("courseId")
+                .setRatingCol("proportionWatched");
+
+        ALSModel model = als.fit(csvData);
+
+        //Para nao NAN em novos usuarios.
+        model.setColdStartStrategy("drop");
+
+        //Dataset<Row> recomendacoesParaUsuario = model.recommendForAllUsers(5);
+        //recomendacoesParaUsuario.show();
+
+        //List<Row> userRecsList = recomendacoesParaUsuario.takeAsList(5);
+        //for (Row r : userRecsList){
+        //    int userId = r.getAs(0);
+        //    String recs = r.getAs(1).toString();
+        //    System.out.println("User " + userId + ". Nos podemos recomendar: " + recs);
+        //    System.out.println("Esse usuario ja assistiu: ");
+        //    csvData.filter("userId = " + userId).show();
+        //}
+
+        Dataset<Row> testData = spark.read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("src/main/resources/VPPcourseViewsTest.csv");
+
+        model.transform(testData).show();
+        model.recommendForUserSubset(testData, 5).show();
     }
 }
